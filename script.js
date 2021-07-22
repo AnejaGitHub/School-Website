36d4a51e1ad0320db286842f42b3d46d57d8d225
Saurabh Singh <72705347+AnejaGitHub@users.noreply.github.com>
2021-07-22 23:17:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggle_bar(){
+    var x=document.getElementById("ul_list");
+    if(x.style.display==="none"){
+        x.style.display="block";
+        
+    }
+    else{
+        x.style.display="none";
+    }
+}
+var a=5;
+console.log("dghf");
+console.log(a);
